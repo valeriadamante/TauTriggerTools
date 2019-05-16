9bb5e06384dfb4fbc1946a132a0879d59dcb294b
hale sert <sert@physik.rwth-aachen.de>
2019-05-16 19:31:31 +0200

    adding new branches for combined HLT decisions of 2018

--- TauTagAndProbe/test/convertTreeForFitting2018.py
+++ TauTagAndProbe/test/convertTreeForFitting2018.py
@@ -1,7 +1,7 @@
 from ROOT import *
 import numpy as n
 
-MC = False
+MC = True
 DYJets = True   # False means WJet enriched cuts will be used, True means cuts for DYJet enriched samples will be used
 
 Sample2017 = False
@@ -18,25 +18,24 @@ else:
 	tauGenMatching = False
 	excludeLumiSections = False
 	print "==> SS events are stored as weights and applied to suppress the bkg for Data samples! <=="
-	print "==> The additional lumi sections in \"Final/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt\" compared to \"ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt\" are removed for Data samples! <=="
 
 disabledPScolumns = False  # True; to remove the disabled columns, False; to consider all columns
 
 # the hadd of all the output ntuples
-path = "/eos/user/h/hsert/TriggerStudies/ForkedRepo/Samples2018/181018/"
-#Final files for the second round of SFs
+path = "/eos/user/h/hsert/TriggerStudies/ForkedRepo/Samples2018/190110/"
+
 if MC:
 	if DYJets:
-		fname =  path + "NTuple_DYJetsToLL_Fall17_12Apr2018_v1Andext1v1_12062018_2018PU.root"
+		fname =  path + "Ntuple_SingleMuon_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_190121_PUreweight1000MCbin.root"
 	if not DYJets:
 		fname =  path + "NTuple_0WJets_12Apr2018_12062018_PU_1000binMC.root"
 else:
-	fname =  path + "Ntuple_Data_Run2018C_PromptRecov1v2v3_181018.root"
+	#fname =  path + "Ntuple_SingleMuon_Run2018ABCDReReco17SepPromptRecoD_190121.root"
+	#fname =  path + "Ntuple_SingleMuon_Run2018D-PromptReco-v2_190121.root"
+	fname = path + "Ntuple_SingleMuon_Run2018C-17Sep2018-v1_190121.root"
+	#fname = "./NTuple_SingleMu_Data_2018_filterTest_Run201.root"
 
-#Final files for the first round of SFs
-#path = "/afs/cern.ch/user/h/hsert/TriggerStudies/ForkedRepo/Samples/2018_01_14/"
-#fname =  path + "NTuple_Data2017BCDEF_17Nov2017-v1_14_01_2018.root"
-#fname =  path +"NTuple_DYJets_RunIIFall17MiniAOD-RECOSIMstep_94X_mc2017_realistic_nomPlusExt_14_01_2018_PU.root"
+
 
 #pt = [20, 26, 30, 34]
 pt = [20, 26, 30, 32, 34]
@@ -45,7 +44,7 @@ if(Sample2017):
 	numberOfHLTTriggersForFit = 26
 if(Sample2018):
 	numberOfHLTTriggers = 27
-	numberOfHLTTriggersForFit = 33
+	numberOfHLTTriggersForFit = 40
 
 
 
@@ -58,7 +57,7 @@ if MC:
 else:
 	suppressionType = "SSsubtraction"
 if DYJets:
-	outname = fname.replace ('.root', '_' + suppressionType + '_MediumWP2017v2_forFit.root')
+	outname = fname.replace ('.root', '_' + suppressionType + '_VVLooseWP2017v2_forFit_260419_test.root')
 else:
 	outname = fname.replace ('.root', '_' + suppressionType + '_WjetEnriched_MediumWP2017v2_forFit.root')
 fOut = TFile (outname, 'recreate')
@@ -83,26 +82,35 @@ for i in range (0, numberOfHLTTriggersForFit):
 	tTriggerNames.GetEntry(i)
 	if(i < numberOfHLTTriggers):
 		name = ("hasHLTPath_" + str(i))
-	#elif(i==numberOfHLTTriggers):
-	#	name = ("hasHLTditauPath20_L1pt32") # HLT_IsoMu24_eta2p1_MediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_CrossL1_v
-	#elif(i==numberOfHLTTriggers+1):
-	#	name = ("hasHLTditauPath20_L1pt34") # HLT_IsoMu24_eta2p1_MediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_CrossL1_v
 	elif(i==numberOfHLTTriggers):
 		name = ("hasHLTetauPath_14HPS")# _IsoMu20_LooseChargedIsoPFTau27_plusL1Tau26andHLTTau30")
 	elif(i==numberOfHLTTriggers+1):
 		name = ("hasHLTmutauPath_14HPS") #_IsoMu20_LooseChargedIsoPFTau27")
 	elif(i==numberOfHLTTriggers+2):
-		name = ("hasHLTditauPath_20HPS")#_TightTau35TightIDor_MediumTau35TightIDplusHLTTau40or_TightTau35plusHLTTau40")
-	elif(i==numberOfHLTTriggers+3):
+                name = ("hasHLTditauPath_15or20HPS")#_TightTau35TightIDor_MediumTau35TightIDplusHLTTau40or_TightTau35plusHLTTau40")
+        elif(i==numberOfHLTTriggers+3):      #HLT_15 for MC and HLT_20 for data
 		name = ("hasHLTetauPath_8noHPS")# _IsoMu20_LooseChargedIsoPFTau27_plusL1Tau26andHLTTau30")
 	elif(i==numberOfHLTTriggers+4):
 		name = ("hasHLTmutauPath_8noHPS") #_IsoMu20_LooseChargedIsoPFTau27")
 	elif(i==numberOfHLTTriggers+5):
 		name = ("hasHLTditauPath_4or5or6noHPS")#_TightTau35TightIDor_MediumTau35TightIDplusHLTTau40or_TightTau35plusHLTTau40")
-
+	elif(i==numberOfHLTTriggers+6):
+		name = ("hasHLTditauPathnoHPS_L1pt32") # HLT_IsoMu24_eta2p1_MediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_CrossL1_v
+	elif(i==numberOfHLTTriggers+7):
+		name = ("hasHLTditauPathnoHPS_L1pt34")
+	elif(i==numberOfHLTTriggers+8):
+		name = ("hasHLTditauPathHPS_L1pt32")
+	elif(i==numberOfHLTTriggers+9):
+		name = ("hasHLTditauPathHPS_L1pt34")
+	elif(i==numberOfHLTTriggers+10):
+		name = ("hasHLTetauPath_8and14")
+	elif(i==numberOfHLTTriggers+11):
+		name = ("hasHLTmutauPath_8and14")
+	elif(i==numberOfHLTTriggers+12):
+		name = ("hasHLTditauPath_4or5or6and15or20")
 	tOut.Branch(name, hltPathTriggered_OS[i], name+"/I")
 	
-	tOut.Branch(name, hltPathTriggered_OS[i], name+"/I")
+	#tOut.Branch(name, hltPathTriggered_OS[i], name+"/I")
 
 #tOut.Branch("isoHLT", hltPathTriggered_OS[6], name+"/I")
 
@@ -151,16 +159,6 @@ for ev in range (0, nentries):
 			hltPathTriggered_OS[bitIndex][0] = 1
 		else:
 			hltPathTriggered_OS[bitIndex][0] = 0
-		"""
-		if ((triggerBits >> bitIndex) & 1) == 1 and (L1pt>=32) :
-			hltPathTriggered_OS[numberOfHLTTriggers][0] = 1
-		else:
-			hltPathTriggered_OS[numberOfHLTTriggers][0] = 0
-		if ((triggerBits >> bitIndex) & 1) == 1 and (L1pt>=34):
-			hltPathTriggered_OS[numberOfHLTTriggers+1][0] = 1
-		else:
-			hltPathTriggered_OS[numberOfHLTTriggers+1][0] = 0
-			"""
 	else:
 		if ((triggerBits >> bitIndex) & 1) == 1:
 			hltPathTriggered_OS[bitIndex][0] = 1
@@ -168,7 +166,7 @@ for ev in range (0, nentries):
 			hltPathTriggered_OS[bitIndex][0] = 0
 
 	if(bitIndex==14):  #mutau
-		if (((triggerBits >> bitIndex) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>30):
+		if (((triggerBits >> bitIndex) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>=30):
 			hltPathTriggered_OS[numberOfHLTTriggers][0] = 1	  # this is the path for etau trigger. So (L1iso) should be applied here!
 		else:
 			hltPathTriggered_OS[numberOfHLTTriggers][0] = 0
@@ -178,14 +176,8 @@ for ev in range (0, nentries):
 		else:
 			hltPathTriggered_OS[numberOfHLTTriggers+1][0] = 0
 
-	if(bitIndex==20):  #ditau
-		if (((triggerBits >> bitIndex) & 1) == 1):
-			hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 1	  # this is the path for etau trigger. So (L1iso) should be applied here!
-		else:
-			hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 0
-
 	if(bitIndex==8):  #mutau
-		if (((triggerBits >> bitIndex) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>30):
+		if (((triggerBits >> bitIndex) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>=30):
 			hltPathTriggered_OS[numberOfHLTTriggers+3][0] = 1	  # this is the path for etau trigger. So (L1iso) should be applied here!
 		else:
 			hltPathTriggered_OS[numberOfHLTTriggers+3][0] = 0
@@ -195,33 +187,108 @@ for ev in range (0, nentries):
 		else:
 			hltPathTriggered_OS[numberOfHLTTriggers+4][0] = 0
 
-    if (((((triggerBits >> 4) & 1) == 1 and HLTpt>40) or (((triggerBits >> 5) & 1) == 1 and HLTpt>40) or (((triggerBits >> 6) & 1) == 1))):
+	# ditau
+    if (((((triggerBits >> 4) & 1) == 1 and HLTpt>=40) or (((triggerBits >> 5) & 1) == 1 and HLTpt>=40) or (((triggerBits >> 6) & 1) == 1))):
 	    hltPathTriggered_OS[numberOfHLTTriggers+5][0] = 1  # this is the path for di-tau trigger. HLTpt cut is required to have the same threshold on tau + L1Pt is needed due to L1 matching differences between MC and Data
+            if(L1pt>=32):
+                    hltPathTriggered_OS[numberOfHLTTriggers+6][0] = 1     # this is the path for ditau trigger for data with L1pt>=32
+            if(L1pt>=34):
+                    hltPathTriggered_OS[numberOfHLTTriggers+7][0] = 1     # this is the path for ditau trigger for data with L1pt>=32
     else:
 	    hltPathTriggered_OS[numberOfHLTTriggers+5][0] = 0
+            hltPathTriggered_OS[numberOfHLTTriggers+6][0] = 0
+            hltPathTriggered_OS[numberOfHLTTriggers+7][0] = 0
+
+    if(MC):  #ditau
+            if (((triggerBits >> 15) & 1) == 1):
+                    hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 1     # this is the path for ditau trigger for MC samples
+                    if(L1pt>=32):
+			    hltPathTriggered_OS[numberOfHLTTriggers+8][0] = 1     # this is the path for ditau trigger for MC samples with L1pt>=32
+                    if(L1pt>=34):
+                            hltPathTriggered_OS[numberOfHLTTriggers+9][0] = 1     # this is the path for ditau trigger for MC samples with L1pt>=34
+            else:
+                    hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 0
+                    hltPathTriggered_OS[numberOfHLTTriggers+8][0] = 0
+                    hltPathTriggered_OS[numberOfHLTTriggers+9][0] = 0
+    elif(not MC):  #ditau
+            if (((triggerBits >> 20) & 1) == 1):
+                    hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 1     # this is the path for ditau trigger for data
+                    if(L1pt>=32):
+			    hltPathTriggered_OS[numberOfHLTTriggers+8][0] = 1     # this is the path for ditau trigger for data with L1pt>=32
+		    if(L1pt>=34):
+                            hltPathTriggered_OS[numberOfHLTTriggers+9][0] = 1     # this is the path for ditau trigger for data with L1pt>=32
+            else:
+                    hltPathTriggered_OS[numberOfHLTTriggers+2][0] = 0
+                    hltPathTriggered_OS[numberOfHLTTriggers+8][0] = 0
+                    hltPathTriggered_OS[numberOfHLTTriggers+9][0] = 0
+
+    if(MC):
+            if (((triggerBits >> 14) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>=30):
+                    hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 1	  # this is the path for etau trigger. So (L1iso) should be applied here!
+            else:
+                    hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 0
+
+            if ((triggerBits >> 14) & 1) == 1:
+                    hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 1 # this is the path for mutau trigger. So no extra requirement is needed like: L1pt and L1iso and HLTpt
+            else:
+                    hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 0
+
+            if (((triggerBits >> 15) & 1) == 1):
+                    hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 1
+            else:
+                    hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 0
+    else:
+            if(RunNumber < 317509):
+                    if (((triggerBits >> 8) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>=30):
+                            hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 1	  # this is the path for etau trigger. So (L1iso) should be applied here!
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 0
+
+                    if ((triggerBits >> 8) & 1) == 1:
+                            hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 1 # this is the path for mutau trigger. So no extra requirement is needed like: L1pt and L1iso and HLTpt
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 0
+
+                    if (((((triggerBits >> 4) & 1) == 1 and HLTpt>=40) or (((triggerBits >> 5) & 1) == 1 and HLTpt>=40) or (((triggerBits >> 6) & 1) == 1))):
+                    # this is the path for di-tau trigger. HLTpt cut is required to have the same threshold on tau + L1Pt is needed due to L1 matching differences between MC and Data
+                            hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 1
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 0
+            else:
+
+                    if (((triggerBits >> 14) & 1) == 1 and L1pt>=26 and L1iso>0 and HLTpt>=30):
+                            hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 1  # this is the path for etau trigger. So (L1iso) should be applied here!
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+10][0] = 0
+
+                    if ((triggerBits >> 14) & 1) == 1:
+                            hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 1 # this is the path for mutau trigger. So no extra requirement is needed like: L1pt and L1iso and HLTpt
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+11][0] = 0
+
+                    if (((triggerBits >> 20) & 1) == 1):
+                            hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 1
+                    else:
+                            hltPathTriggered_OS[numberOfHLTTriggers+12][0] = 0
 
 		
     bkgSubW[0] = 1. if tIn.isOS else -1.
-    #if (L1pt > 26) and (L1iso) and (HLTpt > 32) and (((triggerBits >> 2) & 1) == 1):
-    #    hltPathTriggered_OS[6][0] = 1
-    #else:
-    #    hltPathTriggered_OS[6][0] = 0
 
-    if not "Data" in fname:
+    if not "Run201" in fname:
         puweight = tIn.puweight
     else:
         puweight = 1
 
     bkgSubANDpuW[0] = bkgSubW[0]*puweight
 
-    if(tIn.byMediumIsolationMVArun2017v2DBoldDMwLT2017 > 0.5):
+    if(tIn.byVVLooseIsolationMVArun2017v2DBoldDMwLT2017 > 0.5):
         #Mass cuts, mt and mvis for DY Jets
 	    if DYJets:
 		    if(tIn.mT < 30 and tIn.mVis >40 and tIn.mVis < 80):
-			    if(tauGenMatching):  #for tau gen matching
+				if(tauGenMatching):  #for tau gen matching
 				    if(tIn.tau_genindex > 0):
 					    tOut.Fill()
-			    else:
+				else:
 				    tOut.Fill()
         #High mT requirement for WJets
 	    elif not DYJets:
@@ -231,11 +298,6 @@ for ev in range (0, nentries):
 					    tOut.Fill()
 			    else:
 				    tOut.Fill()
-"""
-if(disabledPScolumns):   # for removing the disabled PS columns:
-    if((RunNumber<305177 and PS_column>=2) or (RunNumber>=305178 and RunNumber<=305387 and PS_column>=2 and PS_column!=10) or (RunNumber>=305388 and PS_column>=3 and PS_column!=11 and PS_column!=12)):
-	    tOut.Fill()
-"""
 
 tOutNames.Write()
 tOut.Write()
