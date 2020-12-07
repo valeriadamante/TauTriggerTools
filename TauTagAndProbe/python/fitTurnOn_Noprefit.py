baf886bb0d521f5d2cd5138f1b81a368235d31dd
VINAYAKRISHNAN <vinaya.krishna@cern.ch>
2020-12-07 17:53:04 +0100

    montonic object added

--- TauTagAndProbe/python/fitTurnOn_Noprefit.py
+++ TauTagAndProbe/python/fitTurnOn_Noprefit.py
@@ -1,7 +1,45 @@
 import ROOT
 ROOT.gROOT.SetBatch(True)
 ROOT.TH1.SetDefaultSumw2()
+import pymc3 as pm
+from gp_monotonic import gp_monotonic
 
 path_prefix = '' if 'TauTriggerTools' in os.getcwd() else 'TauTriggerTools/'
 sys.path.insert(0, path_prefix + 'Common/python')
 from RootObjects import Histogram, Graph
+
+parser = argparse.ArgumentParser(description='Fit turn-on curves.')
+parser.add_argument('--input', required=True, type=str, help="ROOT file with turn-on curves")
+parser.add_argument('--output', required=True, type=str, help="output file prefix")
+parser.add_argument('--channels', required=False, type=str, default='etau,mutau,ditau', help="channels to process")
+parser.add_argument('--decay-modes', required=False, type=str, default='all,0,1,10,11', help="decay modes to process")
+parser.add_argument('--working-points', required=False, type=str,
+                    default='VVVLoose,VVLoose,VLoose,Loose,Medium,Tight,VTight,VVTight',
+                    help="working points to process")
+args = parser.parse_args()
+
+class FitResults:
+    def __init__(self, eff, x_pred):
+        N = eff.x.shape[0]
+        eff = copy.deepcopy(eff)
+        new_y = np.cumsum(res.x)
+        delta = eff.y - new_y
+        eff.y_error_low = np.sqrt(eff.y_error_low ** 2 + delta ** 2)
+        eff.y_error_high = np.sqrt(eff.y_error_high ** 2 + delta ** 2)
+        eff.y = new_y
+        yerr = np.maximum(eff.y_error_low, eff.y_error_high)
+        with pm.Model() as gp_model:
+            l_ = pm.HalfCauchy('l_',20)
+            eta_ = pm.HalfCauchy('eta_',1)
+            s_n = pm.HalfNormal('s_n',1)
+            kernel = (eta_**2)*pm.gp.cov.Matern32(1,l_)
+            gp_default = pm.gp.Marginal(cov_func=kernel)
+            gp_default.marginal_likelihood('lik',data_x.reshape(-1,1),data_y,noise=s_n)
+            trace = pm.sample(2000,tune=2000,cores=2)
+        map_estimate = pm.find_MAP(model=gp_model)
+        gp_mon = gp_monotonic(gp_model,gp_default,yerr,map_estimate,eff.x,eff.y)
+        self.y_pred,y_cov = gp_mon.predict(x_pred,return_cov=True)
+        sigma_orig = np.zeros(N)
+        for n in range(N):
+            idx = np.argmin(abs(x_pred - eff.x[n]))
+            sigma_orig[n] = y_cov[idx]
--- TauTagAndProbe/python/gp_monotonic.py
+++ TauTagAndProbe/python/gp_monotonic.py
@@ -13,7 +13,6 @@ from scipy.special import erfinv,erf,logit,erfc
 import math
 import copy
 import theano.tensor as Te
-<<<<<<< HEAD
 from theano.tensor.nlinalg import matrix_inverse
 import pymc3 as pm
 
@@ -34,24 +33,10 @@ class gp_monotonic:
         xm = [x_temp[pt] for pt in range(len(deriv)) if deriv[pt]<0]
         ym = [y_pred[pt] for pt in range(len(deriv)) if deriv[pt]<0]
 
-=======
-
-class gp_monotonic:
-    def __init__(self,kernel,gp,xvals,ydata):
-        self.kernel = kernel # default kernel
-        self.gp    = gp    # gp object
-        self.xvals = xvals # input labels
-        self.ydata = ydata # data points
-        y_pred = self.gp.predict(np.atleast_2d(xvals).T)
-        deriv = np.gradient(y_pred,xvals)
-        xm = [xvals[pt] for pt in range(len(deriv)) if deriv[pt]<0]
-        ym = [y_pred[pt] for pt in range(len(deriv)) if deriv[pt]<0]
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
         self.xm_ar = np.array(xm,dtype='float32')
         self.ym_ar = np.array(ym,dtype='float32')
         N,M = len(self.xvals),len(self.xm_ar)
         print('No. of virtual points : ',M)
-<<<<<<< HEAD
 
         self.kernel = ConstantKernel() * Matern(length_scale=self.mp['l_'], length_scale_bounds=(10,100), nu=1.5)
         Kff  = self.kernel.__call__(np.atleast_2d(xvals).T,np.atleast_2d(xvals).T)
@@ -109,84 +94,12 @@ class gp_monotonic:
     def predict(self,x_star,return_cov=False):
         K_starf   = self.kernel.__call__(np.atleast_2d(x_star).T,np.atleast_2d(self.x_joint).T)
         K_star2   = self.kernel.__call__(np.atleast_2d(x_star).T,np.atleast_2d(x_star).T)
-=======
-        #Initiation of cavity variables
-        self.vi = np.repeat(np.infty, M)
-        self.mi = np.zeros((M, 1))
-        self.si = np.ones(M)
-        # Initialise posterior with prior
-        sigma2_n = 0.25#np.sqrt(self.mp['s_n'])# The noise value should be named as s_n in the model 
-        m_new = np.zeros(M)
-        v_new = sigma2_n
-        print('v_new ',v_new)
-        #EP approximation of posterior
-        for iteration in range(10):
-            print ("Iteration ", iteration)
-            for i in range(M):
-                print('factor ',i)
-                ######################################################################################
-
-                # Find the cavity distribution parameters.
-                if self.division_zero(self.vi[i], v_new):
-                    cavity_m, cavity_v = self.cavity_vars(self.mi[i], self.vi[i], m_new, v_new)
-                else:
-                    print ("Skipping ", i, " for division of zero when removing factor for cavity funtion iiii")
-                    continue
-                #######################################################################################
-                #update posterior
-                m_new,v_new,Zi = self.compute_moments(self.ym_ar[i], cavity_m,cavity_v)
-                
-                # Update/remove approximate factor fi from q_new  
-                self.mi[i],self.vi[i],self.si[i] = self.update_approximate_factor(cavity_m, m_new, cavity_v, v_new, Zi)
-
-
-            # old_vi = copy.copy(self.vi)
-            # old_mi = copy.copy(self.mi)
-            # old_si = copy.copy(self.si)
-    
-            # #Check for convergence, after iteration
-            # if self.check_convergence(self.mi, self.vi, self.si, old_mi, old_vi, old_si):
-            #     print ("Convergence reached.")
-            #     break
-            
-        #tilda parameters
-        mu_t = self.mi.reshape(1,-1)  
-        v_t  = self.vi
-
-        self.mu_joint,self.sigma_joint = self.joint_vector(self.ydata,mu_t[0],sigma2_n,v_t)
-        self.x_joint = np.concatenate((self.xvals, self.xm_ar), axis=0)
-        self.kernel_new = ConstantKernel() *Matern(length_scale=1.75, length_scale_bounds=(1,2), nu=0.75)
-        
-        Kff   = self.kernel_new.__call__(np.atleast_2d(self.xvals).T,np.atleast_2d(self.xvals).T)
-        Kff1  = self.kernel_new.__call__(np.atleast_2d(self.xvals).T,np.atleast_2d(self.xm_ar).T)
-        Kf1f  = self.kernel_new.__call__(np.atleast_2d(self.xm_ar).T,np.atleast_2d(self.xvals).T)
-        Kf1f1 = self.kernel_new.__call__(np.atleast_2d(self.xm_ar).T,np.atleast_2d(self.xm_ar).T)
-
-        print('K_ff shape : ',Kff.shape)
-        print('K_ff1 shape : ',Kff1.shape)
-        print('K_f1f1 shape : ',Kf1f1.shape)
-        
-        A = np.concatenate((Kff + (sigma2_n)*np.eye(N), Kf1f), axis=0)
-        B = np.concatenate((Kff1, Kf1f1), axis=0)
-        self.K_joint = np.concatenate((A,B),axis=1)
-
-        print('Sucessfully created the montonic object')
-
-    def predict(self,x_star,return_cov=False):
-        K_starf   = self.kernel_new.__call__(np.atleast_2d(x_star).T,np.atleast_2d(self.x_joint).T)
-        K_star2   = self.kernel_new.__call__(np.atleast_2d(x_star).T,np.atleast_2d(x_star).T)
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
 
         print('K_starf shape : ',K_starf.shape)
         print('K_star2 shape : ',K_star2.shape)
         
-<<<<<<< HEAD
         f_pred = np.dot(np.dot(K_starf,np.linalg.inv(self.K_joint+self.S_tilda_joint)),self.mu_tilda_joint)
         f_cov  = K_star2 - np.dot(np.dot(K_starf,np.linalg.inv(self.K_joint+self.S_tilda_joint)),K_starf.T)
-=======
-        f_pred = np.dot(np.dot(K_starf,np.linalg.inv(self.K_joint+self.sigma_joint)),self.mu_joint)
-        f_cov  = K_star2 - np.dot(np.dot(K_starf,np.linalg.inv(self.K_joint+self.sigma_joint)),K_starf.T)
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
 
         if(return_cov==True):
             return f_pred,f_cov
@@ -201,11 +114,7 @@ class gp_monotonic:
         normp_zi = np.exp(-0.5*zi**2 -0.918938533204673)
         mu_hat     = cavity_m + (y_i*cavity_v * normp_zi)/(normc_zi*nu*a)
         sigma_hat  = cavity_v - (cavity_v**2 * normp_zi)/((nu**2 + cavity_v)*normc_zi)*(zi+normp_zi/normc_zi)
-<<<<<<< HEAD
         return mu_hat,sigma_hat,normc_zi
-=======
-        return mu_hat[-1],sigma_hat[-1],normc_zi
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
 
     
     def division_zero(self,v1, v2):
@@ -216,7 +125,6 @@ class gp_monotonic:
             return False
         else:
             return True
-<<<<<<< HEAD
     def compute_cavity(self,mi, vi, m_t, v_t):
         inv_vi,inv_vt = 1.0/vi,1.0/v_t
         cavity_v = 1.0/(inv_vi-inv_vt)
@@ -230,41 +138,6 @@ class gp_monotonic:
         Z_new = Z_hat*np.sqrt(2*np.pi)*np.sqrt(cavity_v+v_hat)*np.exp(0.5*((cavity_m-mu_hat)**2)/(cavity_v+v_hat))
         return mu_new,v_new,Z_new
     
-=======
-    def cavity_vars(self,mi, vi, m_new, v_new):
-        inv_vi = 1. / vi
-        inv_v_new = 1. / v_new
-        cavity_v = 1. / (inv_v_new - inv_vi)
-        cavity_m = cavity_v * (m_new * inv_v_new - mi * inv_vi)
-        return cavity_m, cavity_v
-
-    def update_approximate_factor(self,cavity_m, m_new, cavity_v, v_new, Zi):
-        inv_cavity_v = 1. / cavity_v
-        inv_new_v = 1. / v_new
-        # problem 1
-        # division by zero
-        if self.division_zero(cavity_v, v_new):
-            vi = 1. / (inv_new_v - inv_cavity_v)
-        else:
-            vi = np.infty
-            print ("same variance from removing new approximate factor iiii")
-
-            # problem 2
-            # 0 * infinity
-        if vi == np.infty and (inv_cavity_v == 0 or m_new == cavity_m):
-            mi = cavity_m
-            # mi = vi * (inv_new_v * m_new - inv_cavity_v * cavity_m)
-            print ("0*infinity iiii")
-            print (inv_cavity_v)
-        else:
-            mi = vi * (inv_new_v * m_new - inv_cavity_v * cavity_m)
-
-        si = Zi
-
-        #print(mi, vi ,Zi)
-        return mi[-1], vi ,Zi[-1]
-
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
     def param_dist(self,new, old):
         """Measures distance between two vectors of parameters."""
         new_finfo = np.finfo(new.dtype)
@@ -286,11 +159,7 @@ class gp_monotonic:
     def joint_vector(self,y,mu_t,s2,s2_t):
         n,m = len(y),len(mu_t)
         mu_joint = np.concatenate((y, mu_t), axis=0)
-<<<<<<< HEAD
         s_diag   = np.concatenate((s2,s2_t),axis=0)
-=======
-        s_diag   = np.concatenate((s2*np.ones(n),s2_t),axis=0)
->>>>>>> 42ad9e57f2c86db396aaeed1875967fc81eb2d8e
         s_joint =  np.diag(s_diag)
         print(mu_joint.shape,'  ',s_joint.shape)
         return mu_joint,s_joint
