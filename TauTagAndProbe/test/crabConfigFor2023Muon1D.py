8e2e72b4ef9937bb61f5916d8b5968b0e762a54e
Valeria D'Amante <valeriadamante@gmail.com>
2023-10-13 17:02:33 +0200

    added config files for run2023

---
+++ TauTagAndProbe/test/crabConfigFor2023Muon1D.py
@@ -0,0 +1,31 @@
+from CRABClient.UserUtilities import config
+from CRABAPI.RawCommand import crabCommand
+
+config = config()
+
+config.General.workArea = 'work-area'
+config.General.requestName = "Muon1_Run2023D"
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'produceTuples.py'
+
+config.JobType.maxMemoryMB =2000
+config.JobType.numCores = 1
+
+config.Data.inputDataset = '/Muon1/Run2023D-PromptReco-v2/MINIAOD'
+config.Data.inputDBS = "global"
+config.Data.allowNonValidInputDataset = True
+config.Data.lumiMask = '/afs/cern.ch/work/v/vdamante/public/CMSSW_13_0_3/src/TauTriggerTools/Cert_Collisions2023_366442_370790_Golden.json'
+
+config.General.transferOutputs = True
+config.General.transferLogs = False
+config.Data.publication = False
+
+config.Site.storageSite = 'T2_CH_CERN'
+config.Data.outLFNDirBase = "/store/group/phys_tau/vdamante/run3_tuples/"
+
+
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pyCfgParams = ['period=Run2023', 'isMC=False', 'runDeepTau=True', 'globalTag=130X_dataRun3_HLT_v2']
+config.Data.unitsPerJob = 1000
+config.Data.splitting = "Automatic"
+
